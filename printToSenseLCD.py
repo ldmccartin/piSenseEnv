cb874d513d68e0e064275c9c1b4a966199f7b114
Luke.McCartin <luke.mccartin@lwb.org.au>
2019-08-26 22:19:35 +1000

    formatting

--- printToSenseLCD.py
+++ printToSenseLCD.py
@@ -7,7 +7,7 @@ sense = SenseHat()
 def message_print(sensor_data, sensor_data_type, text_colour_rgb):
     print(grab_data()["time"])
     print(sensor_data_type + " : ", end="")
-    print(float("{0:.1f}".format(sensor_data)),end="")
+    print(float("{0:.1f}".format(sensor_data)), end="")
     sense.set_rotation(0)
     sense.show_message(str(float("{0:.1f}".format(sensor_data))), 0.05 ,text_colour_rgb)
     print("\n")
